e33360519b1a0368430d3aed17602cef6e8397c5
Lars Blumberg <lars.blumberg@talea.de>
2019-08-12 22:53:41 +0200

    Support underscores in app module name

--- lib/src/auth0_utilities.dart
+++ lib/src/auth0_utilities.dart
@@ -1,5 +1,5 @@
 import 'dart:io' show Platform;
 
 String callbackUri({String bundleId, String domain}) {
-  return '${bundleId.toLowerCase()}://$domain/${Platform.isIOS ? 'ios' : 'android'}/$bundleId/callback';
+  return '${bundleId.toLowerCase().replaceAll('_', '')}://$domain/${Platform.isIOS ? 'ios' : 'android'}/$bundleId/callback';
 }
