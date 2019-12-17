3c0552398e824ce888d712485695235719edba40
Carlos Esquivel <carlos@katooapp.com>
2019-12-17 15:43:32 +0100

    Upgrate to Flutter 1.12.
    Upgrade Gradle wrapper to 5.1.1. Migrate example to AndroidX. Clean-up ignored files.

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,12 +3,10 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-#import <flutter_auth0/FlutterAuth0Plugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FlutterAuth0Plugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterAuth0Plugin"]];
 }
 
 @end
