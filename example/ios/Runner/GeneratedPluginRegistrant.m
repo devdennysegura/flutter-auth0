2e8cf54c8b52a7efb6d62f2355ddb2e62d8ef616
Carlos Esquivel <carlos@katooapp.com>
2019-12-26 11:08:25 +0100

    Revert unexpected changes

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <flutter_auth0/FlutterAuth0Plugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterAuth0Plugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterAuth0Plugin"]];
 }
 
 @end
